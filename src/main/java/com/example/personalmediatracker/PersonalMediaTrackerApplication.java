758d59cb9e2fb2f596606197f26e76b6f9aa5b9a
SKharel10 <sagarkharel001@gmail.com>
2026-01-04 17:06:12 +0000

    initial commit, spring boot setup

---
+++ src/main/java/com/example/personalmediatracker/PersonalMediaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalmediatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalMediaTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalMediaTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/personalmediatracker/PersonalMediaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalmediatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalMediaTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
