44fa7d61415b5a2bc14254f4d448fd2526fd1f40
SKharel10 <sagarkharel001@gmail.com>
2026-01-04 18:53:23 +0000

    Created MediaItemService along with get and get by id methods

--- src/main/java/com/example/personalmediatracker/service/MediaItemService.java
+++ src/main/java/com/example/personalmediatracker/service/MediaItemService.java
@@ -1,3 +1,23 @@
 package com.example.personalmediatracker.service;
 
-public class MediaItemService {}
+
+import com.example.personalmediatracker.model.MediaItem;
+import com.example.personalmediatracker.repository.MediaItemRepository;
+
+import java.util.List;
+
+public class MediaItemService {
+    private final MediaItemRepository mediaItemRepository;
+
+    public MediaItemService(MediaItemRepository mediaItemRepository) {
+        this.mediaItemRepository = mediaItemRepository;
+    }
+
+    public List<MediaItem> getMediaItems(){
+        return mediaItemRepository.findAll();
+    }
+    public MediaItem getMediaItem(Long id){
+        return mediaItemRepository.findById(id)
+                .orElseThrow( () -> new IllegalArgumentException(id + "not found"));
+    }
+}
