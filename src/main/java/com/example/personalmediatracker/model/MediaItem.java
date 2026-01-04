8af37178d86a592c233d3ccc1dff779853e55ce8
SKharel10 <sagarkharel001@gmail.com>
2026-01-04 17:34:25 +0000

    Updated MediaItem in order with necessary fields and getters and setter methods alongside JPA annotations

--- src/main/java/com/example/personalmediatracker/model/MediaItem.java
+++ src/main/java/com/example/personalmediatracker/model/MediaItem.java
@@ -1,5 +1,58 @@
 package com.example.personalmediatracker.model;
 
-public class MediaItem {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class MediaItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private MediaType mediaType;
+    private Long totalUnits; // Example: 900 pages for a book, 10 episodes for a TV show.
+    private ProgressStatus progressStatus;
+
+    public MediaItem() {
+    }
+
+    public MediaItem(Long id, MediaType mediaType, Long totalUnits, ProgressStatus progressStatus) {
+        this.id = id;
+        this.mediaType = mediaType;
+        this.totalUnits = totalUnits;
+        this.progressStatus = progressStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MediaType getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public Long getTotalUnits() {
+        return totalUnits;
+    }
+
+    public void setTotalUnits(Long totalUnits) {
+        this.totalUnits = totalUnits;
+    }
+
+    public ProgressStatus getProgressStatus() {
+        return progressStatus;
+    }
+
+    public void setProgressStatus(ProgressStatus progressStatus) {
+        this.progressStatus = progressStatus;
+    }
 }
